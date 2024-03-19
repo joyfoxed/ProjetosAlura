f0470c6e22216aababe0fb1a73af4e93ef1b379d
Joesley Cálita <joy_foxed@hotmail.com>
2024-03-19 17:58:17 -0300

    Inicio JS e botoes

---
+++ Encriptador de texto/app.js
@@ -0,0 +1,45 @@
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+function cript(texto) {
+    let criptografia = [ ["e", "enter"], ["i", "imes"] , ["a","ai"],["o", "ober"],["u", "ufat"]];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < criptografia.length; i++) {
+        if (texto.includes(criptografia[i][0])) {
+            texto = texto.replaceAll(criptografia[i][0], criptografia[i][1]) ;
+        }
+    }
+    return texto;
+}
+
+function decript(texto) {
+    let criptografia = [ ["e", "enter"], ["i", "imes"] , ["a","ai"],["o", "ober"],["u", "ufat"]];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < criptografia.length; i++) {
+        if (texto.includes(criptografia[i][1])) {
+            texto = texto.replaceAll(criptografia[i][1], criptografia[i][0]) ;
+        }
+    }
+    return texto;
+}
+
+function btn_cript(){
+    const texto = cript(textArea.value);
+    mensagem.value = texto;
+    textArea.value = "";
+}
+
+function btn_decript(){
+    const texto = decript(textArea.value);
+    mensagem.value = texto;
+    textArea.value = "";
+}
+
